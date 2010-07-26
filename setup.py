07bc33a4dd3c11486015634e0a0c8830e7f2fcca
Dan Jacob <danjac354@gmail.com>
2010-07-26 14:59:22 +0100

    moved to 0.3.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Mail',
-    version='0.3.1',
+    version='0.3.2',
     url='http://bitbucket.org/danjac/flask-mail',
     license='BSD',
     author='Dan Jacob',
