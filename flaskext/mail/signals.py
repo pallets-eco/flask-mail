7dcb532cb46d99ddc39df63ecdbefc8efce006ce
Dan Jacob <danjac354@gmail.com>
2010-08-07 00:04:49 +0100

    signal doc
    --HG--
    branch : refactoring

--- flaskext/mail/signals.py
+++ flaskext/mail/signals.py
@@ -1,8 +1,14 @@
 
 try:
+    
     import blinker
     signals = blinker.Namespace()
-    email_dispatched = signals.signal("email-dispatched")
+    
+    email_dispatched = signals.signal("email-dispatched", doc="""
+Signal sent when an email is dispatched. This signal will also be sent
+in testing mode, even though the email will not actually be sent.
+    """)
+
 except ImportError:
     email_dispatched = None
 
