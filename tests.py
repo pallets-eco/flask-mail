e60807b057a4af646d281346e1b75d2fdc97a16c
James Mapes <mapesj@gmail.com>
2013-04-25 11:22:29 -0700

    Set default sender when message is created. Moved message validations to Connection.send(), validates bulk & individual sending.

--- tests.py
+++ tests.py
@@ -54,7 +54,7 @@ class TestMessage(TestCase):
     def test_initialize(self):
         msg = Message(subject="subject",
                       recipients=["to@example.com"])
-        self.assertEqual(msg.sender, None)
+        self.assertEqual(msg.sender, self.app.extensions['mail'].default_sender)
         self.assertEqual(msg.recipients, ["to@example.com"])
 
     def test_recipients_properly_initialized(self):
@@ -402,3 +402,23 @@ class TestConnection(TestCase):
             self.assertEqual(len(outbox), 100)
             sent_msg = outbox[0]
             self.assertEqual(sent_msg.sender, self.app.extensions['mail'].default_sender)
+
+    def test_send_without_sender(self):
+        self.app.extensions['mail'].default_sender = None
+        msg = Message(subject="testing", recipients=["to@example.com"], body="testing")
+        with self.mail.connect() as conn:
+            self.assertRaises(AssertionError, conn.send, msg)
+
+    def test_send_without_recipients(self):
+        msg = Message(subject="testing",
+                      recipients=[],
+                      body="testing")
+        with self.mail.connect() as conn:
+            self.assertRaises(AssertionError, conn.send, msg)
+
+    def test_bad_header_subject(self):
+        msg = Message(subject="testing\n\r",
+                      body="testing",
+                      recipients=["to@example.com"])
+        with self.mail.connect() as conn:
+            self.assertRaises(BadHeaderError, conn.send, msg)
