90f1a7cc107192962876b1cfb2ab0cc0ef4b9fcb
Stéphane Klein <contact@stephane-klein.info>
2014-10-20 15:21:55 +0200

    Append test for issue #88

--- tests.py
+++ tests.py
@@ -428,7 +428,12 @@ class TestMessage(TestCase):
         msg = Message(subject=make_lazy_string(lambda a: a, u"sübject"),
                       sender='from@example.com',
                       recipients=["to@example.com"])
-        self.assertIn('=?utf-8?q?s=C3=BCbject?=', msg.as_string())
+        self.assertIn(b'=?utf-8?q?s=C3=BCbject?=', msg.as_bytes())
+
+        msg = Message(subject=make_lazy_string(lambda a: a, u"[Foo Bar] Voici vos paramètres d'accès à"),
+                      sender='from@example.com',
+                      recipients=["to@example.com"])
+        self.assertIn(b'=?utf-8?b?W0ZvbyBCYXJdIFZvaWNpIHZvcyBwYXJhbcOodHJlcyBkJ2FjY8OocyDDoA==?=', msg.as_bytes())
 
     def test_extra_headers(self):
         msg = Message(sender="from@example.com",
@@ -691,3 +696,6 @@ class TestConnection(TestCase):
                     msg.mail_options,
                     msg.rcpt_options
                 )
+
+if __name__ == '__main__':
+    unittest.main()
