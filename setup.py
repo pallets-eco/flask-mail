72c7946bc7f86c9314de166136b352075ff2cb10
Will Lachance <wlach@protonmail.com>
2024-05-20 15:17:55 -0400

    Add pre-commit, style linting with ruff (#208)
    * Add pre-commit, style linting with ruff
    * Ruff fixes (and related)
    * Remove README.rst
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -11,37 +11,35 @@ Links
 
 * `documentation <http://packages.python.org/Flask-Mail>`_
 """
+
 from setuptools import setup
 
-
 setup(
-    name='Flask-Mail',
-    version='0.9.1',
-    url='https://github.com/rduplain/flask-mail',
-    license='BSD',
-    author='Dan Jacob',
-    author_email='danjac354@gmail.com',
-    maintainer='Ron DuPlain',
-    maintainer_email='ron.duplain@gmail.com',
-    description='Flask extension for sending email',
+    name="Flask-Mail",
+    version="0.9.1",
+    url="https://github.com/rduplain/flask-mail",
+    license="BSD",
+    author="Dan Jacob",
+    author_email="danjac354@gmail.com",
+    maintainer="Ron DuPlain",
+    maintainer_email="ron.duplain@gmail.com",
+    description="Flask extension for sending email",
     long_description=__doc__,
-    py_modules=[
-        'flask_mail'
-    ],
+    py_modules=["flask_mail"],
     zip_safe=False,
-    platforms='any',
+    platforms="any",
     install_requires=[
-        'Flask',
-        'blinker',
+        "Flask",
+        "blinker",
     ],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-    ]
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )
