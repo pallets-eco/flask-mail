cb584da08181e61bc467f8fd75dead0c08155cdc
David Lord <davidism@gmail.com>
2024-05-21 15:03:51 -0400

    Update tests/conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,21 +3,17 @@ from flask import Flask
 
 from flask_mail import Mail
 
-TESTING_FLASK_APP_CONFIG = {
-    "TESTING": True,
-    "MAIL_DEFAULT_SENDER": "support@mysite.com",
-}
-
+@pytest.fixture
+def app():
+    app = Flask(__name__)
+    app.config.update({
+        "TESTING": True,
+        "MAIL_DEFAULT_SENDER": "support@example.com",
+    })
+    
+    with app.test_request_context():
+        yield app
 
 @pytest.fixture
-def flask_mail(
-    scope="module",
-):
-    app = Flask(__name__)
-    app.config.update(**TESTING_FLASK_APP_CONFIG)
-    assert app.testing is True
-    mail = Mail(app)
-    ctx = app.test_request_context()
-    ctx.push()
-    yield app, mail
-    ctx.pop()
+def mail(app):
+    return Mail(app)
